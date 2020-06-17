da780dde84b29da91837825f4710154def419897
azientar <azientar@users.noreply.github.com>
2020-06-17 11:59:25 -0400

    Add Router, UAL, UAL Scatter, UAL Anchor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link, 
+  userParams,
+  useRouteMatch
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+
+
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <footer></footer>
+      </div>
+    );
+  }
 }
 
 export default App;

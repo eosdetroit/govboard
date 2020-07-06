a881db6db3e894565272b5fcaa71b0baf3d3b70b
Robert Konsdorf <rob@eosdetroit.io>
2020-07-06 14:16:18 -0400

    Remove overview from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,22 +17,6 @@ class Home extends React.Component {
     }
     return (
       <div className="home main-content">
-        <h1>Overview</h1>
-        <br></br>
-        <p>
-          The Office of Inspector General (OIG) is a watchdog agency for the WAX network 
-          with the mission of evaluating WAX Guilds using an evolving heuristic. The OIG determines 
-          which service providers are best suited to operate the network based on competence, 
-          value add, and other metrics. The OIG is staffed by a committee of 3 elected inspector generals (IGs)
-          who publish a rating report ranking the Guilds on a monthly basis. The ratings can be used by WAX 
-          token holders to make voting decisions. The aggregated WAX votes determine the rank of Guilds in 
-          the block production rotation. The top 21 ranked Guilds are the "active" block producers, rank 22 
-          and higher Guilds that are passing minimum requirements are "standby." Among the responsibilities 
-          of the OIG is overseeing and ensuring the continued evolution of the evaluations framework to 
-          improve Guild rating accuracy, identify and remove operational inefficiencies, and promote healthy 
-          competition among Guilds.
-        </p>
-        <br></br>
         <h1>OIG Election Process</h1>
         <br></br>
         <p>

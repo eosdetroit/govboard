bab3e3f75ca42d00441863c17db4fecb4495b74d
Robert Konsdorf <rob@eosdetroit.io>
2020-07-08 22:12:40 -0400

    Add OIG logo to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Redirect
 } from "react-router-dom";
-
+import oigLogo from '../assets/waxOIGlogo.jpg'
 import '../App.css';
 
 class Home extends React.Component {
@@ -18,42 +18,45 @@ class Home extends React.Component {
     }
     return (
       <div className="home main-content" id="main">
-        <h1>OIG Election Process</h1>
-        <br></br>
-        <p>
-          The OIG was started by 3 WAX-appointed inspector generals that currently serve the office. 
-          Each Appointed IG is slated to serve at minimum a 9 month tenure; July 2020 will be the 9th OIG 
-          Report, and thus time to start rotating in Elected IGs using a stake-weighted voting system. 
-          Appointed IGs will leave office on a 3 month rolling basis, and be replaced by Elected IGs who
-          will serve an 18 month tenure and be put up for election on a 6 month rolling basis. There is 
-          one caveat in that an Interim IG needs to be elected to serve a 9 month term in order to migrate 
-          to the 18 month schedule.
-        </p>
-        <br></br>
+        <img class="oigLogo" src={oigLogo} alt="WAX OIG Logo" />
+        <div class="bodyText">
+          <h1>OIG Election Process</h1>
+          <br></br>
+          <p>
+            The OIG was started by 3 WAX-appointed inspector generals that currently serve the office. 
+            Each Appointed IG is slated to serve at minimum a 9 month tenure; July 2020 will be the 9th OIG 
+            Report, and thus time to start rotating in Elected IGs using a stake-weighted voting system. 
+            Appointed IGs will leave office on a 3 month rolling basis, and be replaced by Elected IGs who
+            will serve an 18 month tenure and be put up for election on a 6 month rolling basis. There is 
+            one caveat in that an Interim IG needs to be elected to serve a 9 month term in order to migrate 
+            to the 18 month schedule.
+          </p>
+          <br></br>
 
-        <h1>Election Timeline</h1>
-        <br></br>
-        <p>
-          <b>July 13 2020:</b> Begin candidate nominations.
+          <h1>Election Timeline</h1>
           <br></br>
-          <b>July 20 2020:</b> End nominations and begin election period.
-          <br></br>
-          <b>July 31 2020:</b> Election period ends.
-          <br></br>
-          <b>August 1 2020:</b> Appointed IG replaced with Elected IG.
-          <br></br>
-          <b>October 2020:</b> Election period to determine Interim IG.
-          <br></br>
-          <b>November 1 2020:</b> Appointed IG replaced with Elected Interim IG.
-          <br></br>
-          <b>January 2021:</b> Election period to determine Elected IG.
-          <br></br>
-          <b>February 1 2021:</b> Appointed IG replaced with Elected IG.
-          <br></br>
-          <b>July 2021:</b> Election period with Elected IG.
-          <br></br>
-          <b>August 1 2021:</b> Interim IG replaced with Elected IG.
-        </p>
+          <p>
+            <b>July 13 2020:</b> Begin candidate nominations.
+            <br></br>
+            <b>July 20 2020:</b> End nominations and begin election period.
+            <br></br>
+            <b>July 31 2020:</b> Election period ends.
+            <br></br>
+            <b>August 1 2020:</b> Appointed IG replaced with Elected IG.
+            <br></br>
+            <b>October 2020:</b> Election period to determine Interim IG.
+            <br></br>
+            <b>November 1 2020:</b> Appointed IG replaced with Elected Interim IG.
+            <br></br>
+            <b>January 2021:</b> Election period to determine Elected IG.
+            <br></br>
+            <b>February 1 2021:</b> Appointed IG replaced with Elected IG.
+            <br></br>
+            <b>July 2021:</b> Election period with Elected IG.
+            <br></br>
+            <b>August 1 2021:</b> Interim IG replaced with Elected IG.
+          </p>
+        </div>
       </div>
     );
   }

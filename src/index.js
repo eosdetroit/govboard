b45322ac20b75b5619d8eaa5fe11f7a7f007f1ad
Robert Konsdorf <rob@eosdetroit.io>
2020-07-14 16:30:52 -0400

    Revert "Rip out scatter"
    This reverts commit f2143d3912df7d1d60a2ec30b11c8e6a2765a493.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { UALProvider, withUAL } from 'ual-reactjs-renderer';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+import { Scatter } from 'ual-scatter';
 import { Anchor } from 'ual-anchor';
 import { Wax } from '@eosdacio/ual-wax';
 
@@ -29,14 +30,14 @@ const waxTestnet = {
 }
 */
 
+const scatter = new Scatter([waxMainnet], { appName: 'WAX OIG Governance Dashboard' });
 const anchor = new Anchor([waxMainnet], { appName: 'WAX OIG Governance Dashboard' });
 const waxcloud = new Wax([waxMainnet], { appName: 'WAX OIG Governance Dashboard' });
 
 const UALConsumer = withUAL(App);
 
 ReactDOM.render(
-  <UALProvider chains={[waxMainnet]} authenticators={[waxcloud, anchor]} 
-    appName={'WAX OIG Governance Dashboard'}>
+	<UALProvider chains={[waxMainnet]} authenticators={[waxcloud, anchor, scatter]} appName={'WAX OIG Governance Dashboard'}>
     	<Router>
           <UALConsumer />
       </Router>

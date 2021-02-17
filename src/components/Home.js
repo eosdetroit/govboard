7951aca6b4f4e35b3b7c7057cb4a268f8cfa5633
Karyne Mayer <mayer.karyne@gmail.com>
2021-02-17 20:06:06 -0300

    Styles home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,195 @@
+/** @jsx jsx */
+
 import React from 'react';
+import Crest from '../assets/crest.svg';
+import Hive from '../assets/hive.svg';
+import Bees from '../assets/bees.svg';
+import BeePack from '../assets/bee-pack.svg';
+import ItemPolygon from '../assets/itemPolygon.svg';
 import oigLogo from '../assets/waxOIGTransparent-small.png';
-import '../App.css';
+
+import { jsx } from '@emotion/react';
+import * as GLOBAL_STYLE from '../theme';
 
 class Home extends React.Component {
 
-  render() {
-    return (
-      <div className="home main-content" id="main">
-        <div className="badge"><img className="oigLogo" src={oigLogo} alt="WAX OIG Logo" /></div>
-        <div className="bodyText">
-          <h1>OIG Election Process</h1>
-          <br />
-          <p>
-            The OIG was started by 3 WAX-appointed inspector generals that currently serve the office. 
-            Each Appointed IG is slated to serve at minimum a 9 month tenure; July 2020 will be the 9th OIG 
-            Report, and thus time to start rotating in Elected IGs using a stake-weighted voting system. 
-            Appointed IGs will leave office on a 3 month rolling basis, and be replaced by Elected IGs who
-            will serve an 18 month tenure and be put up for election on a 6 month rolling basis.
-          </p>
-          <br />
-          <p>
-            Each OIG election consists of a nomination phase, where nominees can accept or decline, and provide additional
-            information. This is followed by a voting phase, where WAX token holders can vote on one OIG candidate. After 
-            the voting phase is concluded, the winner will be determined as the candidate with the most votes on the ballot.
-          </p>
-          <br />
-
-          <h1>Current Election Timeline</h1>
-          <br />
-          <p>
-            <strong>January 8th, 2021 0:00:00 UTC:</strong> Begin candidate nominations.
-            <br />
-            <strong>January 18th, 2021 0:00:00 UTC:</strong> End nominations and begin election period.
-            <br />
-            <strong>January 24th, 2021 23:59:59 UTC:</strong> Election period ends.
-            <br />
-            <strong>February 1st, 2021 00:00:00 UTC:</strong> Appointed IG replaced with Elected IG.
-          </p>
-         <br />
-          <h1>Future Election Schedule</h1>
-          <br />
-          <p>
-            <b>July 2021:</b> Election period to replace first Interim IG.
-            <br />
-            <b>August 1 2021:</b> Interim IG replaced with Elected IG.
-          </p>
-        </div>
-      </div>
-    );
+    render() {
+        return (
+            <div id="main"
+                css={{
+                    padding: `${GLOBAL_STYLE.spacing.l} 0`,
+                }}
+            >
+                <div
+                    css={{
+                        minHeight: '80vh',
+                        backgroundImage: `url(${Hive})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'contain',
+                        backgroundPositionX: '-40px',
+                        position: 'relative',
+                        [GLOBAL_STYLE.mediaQuery.tabletUp]: {
+                            minHeight: '100vh',
+                        },
+                        '& .headline': {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        },
+                        '& .crest': {
+                            marginRight: GLOBAL_STYLE.spacing.s,
+                            marginLeft: GLOBAL_STYLE.spacing.s,
+                            width: '80vw',
+                            objectFit: 'contain',
+                        },
+                        '& .bees': {
+                            display: 'none',
+                            [GLOBAL_STYLE.mediaQuery.tabletLandscapeUp]: {
+                                display: 'block',
+                                position: 'absolute',
+                                right: GLOBAL_STYLE.spacing.s,
+                                top: '0',
+                            },
+                        },
+                        '& .headlineTitle, & .headlineParagraph': {
+                            textAlign: 'center',
+                            marginRight: GLOBAL_STYLE.spacing.s,
+                            marginLeft: GLOBAL_STYLE.spacing.s,
+                        },
+                        '& .nextElection': {
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            margin: `${GLOBAL_STYLE.spacing.xxl} ${GLOBAL_STYLE.spacing.s}`,
+                        },
+                        '& .beePack': {
+                            height: 90,
+                            objectFit: 'contain',
+                            [GLOBAL_STYLE.mediaQuery.largeMobileUp]: {
+                                height: 150,
+                            }
+                        },
+                        '& .nextElectionDate': {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        },
+                        '& .electionDateHeadline': {
+                            fontWeight: 400,
+                            marginBottom: GLOBAL_STYLE.spacing.xxs,
+                            textAlign: 'center',
+                        },
+                        '& .electionMonth': {
+                            marginTop: '0',
+                            textAlign: 'center',
+                        },
+                    }}
+                >
+                    <div className="headline">
+                        <img src={Crest} className="crest" alt="WAX Office of Inspector General crest." />
+                        <GLOBAL_STYLE.H1 className="headlineTitle">
+                            A watchdog agency for the WAX network
+                        </GLOBAL_STYLE.H1>
+                        <GLOBAL_STYLE.P className="headlineParagraph">
+                            evaluating WAX Guilds using an evolving heuristics.
+                        </GLOBAL_STYLE.P>
+                        <div className="nextElection">
+                            <img
+                                src={BeePack}
+                                className="beePack"
+                                alt="Pack of NFTS with a bee drawed in the center."
+                            />
+                            <div className="nextElectionDate">
+                                <GLOBAL_STYLE.H4 className="electionDateHeadline">Next elections</GLOBAL_STYLE.H4>
+                                <GLOBAL_STYLE.H1 className="electionMonth">July 2021</GLOBAL_STYLE.H1>
+                            </div>
+                            <img
+                                src={BeePack}
+                                className="beePack"
+                                alt="Pack of NFTS with a bee drawed in the center."
+                            />
+                        </div>
+                        <img src={Bees} className="bees" alt="Fours bees flying upward." />
+                    </div>
+                </div>
+                <div
+                    css={{
+                        display: 'flex',
+                        flexDirection: 'column-reverse',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginRight: GLOBAL_STYLE.spacing.s,
+                        marginLeft: GLOBAL_STYLE.spacing.s,
+                        [GLOBAL_STYLE.mediaQuery.tabletUp]: {
+                            flexDirection: 'row',
+                            justifyContent: 'space-evenly',
+                        },
+                        '& .oigLogo': {
+                            width: '30vw',
+                            objectFit: 'contain',
+                            marginTop: GLOBAL_STYLE.spacing.s,
+                        },
+                        '& .electionProcess': {
+                            [GLOBAL_STYLE.mediaQuery.tabletUp]: {
+                                marginLeft: GLOBAL_STYLE.spacing.xl,
+                            },
+                        },
+                        '& .electionProcessTitle': {
+                            marginBottom: GLOBAL_STYLE.spacing.m,
+                        },
+                        '& .electionStep': {
+                            borderBottom: `1px solid ${GLOBAL_STYLE.colors.blue04}`,
+                            marginTop: GLOBAL_STYLE.spacing.s,
+                        },
+                        '& .electionStepName': {
+                            display: 'flex',
+                            alignItems: 'center',
+                            marginBottom: GLOBAL_STYLE.spacing.xs,
+                        },
+                        '& .itemPolygon': {
+                            marginRight: GLOBAL_STYLE.spacing.xs,
+                        },
+                        '& .electionStepNameText': {
+                            marginBottom: 0,
+                        },
+                        '& .electionStepContent': {
+                            marginBottom: GLOBAL_STYLE.spacing.s,
+                        }
+                    }}
+                >
+                    <img src={oigLogo} className="oigLogo" alt="WAX Office of Inspector General logo." />
+                    <div className="electionProcess">
+                        <GLOBAL_STYLE.H3 className="electionProcessTitle">The Election Process</GLOBAL_STYLE.H3>
+                        <div className="electionStep">
+                            <div className="electionStepName">
+                                <img src={ItemPolygon} className="itemPolygon" alt="" />
+                                <GLOBAL_STYLE.H4 className="electionStepNameText">The nomination phase</GLOBAL_STYLE.H4>
+                            </div>
+                            <GLOBAL_STYLE.P className="electionStepContent">
+                                In this phase the nominees can accept or decline, and provide additional information.{' '}
+                            </GLOBAL_STYLE.P>
+                        </div>
+                        <div className="electionStep">
+                            <div className="electionStepName">
+                                <img src={ItemPolygon} className="itemPolygon" alt="" />
+                                <GLOBAL_STYLE.H4 className="electionStepNameText">The voting phase</GLOBAL_STYLE.H4>
+                            </div>
+                            <GLOBAL_STYLE.P className="electionStepContent">WAX token holders can vote on one OIG candidate.</GLOBAL_STYLE.P>
+                        </div>
+                        <div className="electionStep">
+                            <div className="electionStepName">
+                                <img src={ItemPolygon} className="itemPolygon" alt="" />
+                                <GLOBAL_STYLE.H4 className="electionStepNameText">The elected IG</GLOBAL_STYLE.H4>
+                            </div>
+                            <GLOBAL_STYLE.P className="electionStepContent">
+                                The winner will be determined as the candidate with the most votes on the ballot. This
+                                person will replace the interim IG at the beginning of the following month.
+                            </GLOBAL_STYLE.P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
   }
 }
 
